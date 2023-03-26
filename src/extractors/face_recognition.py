738cd3c9a12928c5e0f626ec5353c674ef8e0fab
al3xkras <alexandr.krasovskiy@gmail.com>
2023-03-26 10:35:39 +0000

    Create class: FaceVectorExtractor

--- src/extractors/face_recognition.py
+++ src/extractors/face_recognition.py
@@ -1,11 +1,28 @@
-import face_recognition
+import face_recognition as fr
 
-from .face_recognition import FaceVector
+from src.commons.face_recognition import FaceVector
+from PIL.Image import Image
+from typing import SupportsIndex
+
+
 
 class FaceVectorExtractor:
-    def __init__(self, bitmap):
-        self.bitmap=bitmap
-
-    def get_face_vector(self) -> FaceVector:
+    def __init__(self, *args):
         pass
+
+    @staticmethod
+    def get_face_bounding_box(img: Image) -> SupportsIndex[int, int, int, int]:
+        """
+        Returns a bounding box of a human face in an image
+        (if an image contains >1 or 0 faces, raise a runtime Exception)
+        """
+        return fr.face_locations(img)
+
+    @classmethod
+    def get_face_image(cls,img: Image) -> Image:
+        """
+        :return: cropped to a face bounding box image
+        """
+        bbox = cls.get_face_bounding_box(img)
+        return img.crop(bbox)
 
