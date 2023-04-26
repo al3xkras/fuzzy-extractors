f0b6f122492319817cc0e1ce240a43417715480b
al3xkras <alexandr.krasovskiy@gmail.com>
2023-04-26 07:02:13 +0000

    main.py: implement method extract_video_faces

---
+++ main.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from extractors import FrameIterator, FaceVectorExtractor
+
+names = [
+    "NileRed",
+    "HikaruNakamura"
+]
+
+
+def extract_video_faces(name: str):
+    fi = FrameIterator("./videos/%s.mp4" % name)
+    ex = FaceVectorExtractor()
+    i = 0
+    i_max = 2000
+
+    def consumer(img: np.ndarray):
+        global i
+        if i > i_max:
+            return False
+        i += 1
+        if i % 100 != 0:
+            return True
+        try:
+            face = ex.get_face_image(img)
+            fi.save_image(face, "./images/%s%d.jpg" % (name, i))
+        except ValueError:
+            return True
+        return True
+
+    fi.iterate_images(consumer)
+
+
+if __name__ == '__main__':
+    pass
